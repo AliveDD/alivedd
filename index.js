967f7337c7d5ff293dfc3d675d946dba59d2c64d
Eugene <e.balashov@rambler-co.ru>
2017-10-30 13:24:49 +0300

    add cache functions

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+export function createCache(name, value) {
+  if (isLocalStorageAvailable()) {
+    window.localStorage.setItem(name, value);
+  }
+}
+
+export function readCache(name) {
+  let output = false;
+  if (isLocalStorageAvailable()) {
+    output = window.localStorage.getItem(name);
+  }
+  return output;
+}
+
+export function eraseCache(name) {
+  if (isLocalStorageAvailable()) {
+    window.localStorage.removeItem(name);
+  }
+}
+
+
+function isLocalStorageAvailable() {
+  let output;
+  var test = 'test';
+  try {
+    localStorage.setItem(test, test);
+    localStorage.removeItem(test);
+    output = true;
+  } catch (e) {
+    output = false;
+  }
+  return output;
+}
